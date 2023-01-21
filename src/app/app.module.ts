d1b05402aa8249324255288b122841a71bbb9b4a
ahmidev <ahmid.aitouali@gmail.com>
2023-01-21 17:06:55 +0100

    transfert du code de l'original

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FinalComponent } from './final/final.component';
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { ParameterComponent } from './parameter/parameter.component';
+import { PlayerComponent } from './player/player.component';
+import { WheelComponent } from './wheel/wheel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FinalComponent,
+    GameComponent,
+    HomeComponent,
+    ParameterComponent,
+    PlayerComponent,
+    WheelComponent
   ],
   imports: [
     BrowserModule,
